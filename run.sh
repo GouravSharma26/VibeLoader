19d921db215fda31592a89828040bfa67d566d19
GouravSharma26 <gourav123sharma4567@gmail.com>
2026-03-31 02:01:55 +0530

    Fix: Added migrate command to create database tables

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-# Start Celery worker in the background
+# Exit immediately if a command exits with a non-zero status
+set -o errexit
+
+# 1. Create the database tables in the cloud
+python manage.py migrate
+
+# 2. Start Celery worker in the background
 celery -A ytdownloader worker --loglevel=info --pool=solo &
 
-# Start Gunicorn (Django) in the foreground
+# 3. Start Gunicorn (The Web Server)
 gunicorn ytdownloader.wsgi:application --bind 0.0.0.0:$PORT
